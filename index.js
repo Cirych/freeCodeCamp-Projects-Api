69bac3583a34a34b2112401ccbcd88cd99050513
Cirych <cirych@mail.ru>
2016-11-24 15:21:52 +0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const {PORT} = process.env
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT || 3000, function () {
+  console.log('app started')
+})
