bd82ffa383a0950cb4028c25535b9ee6a80342c3
Cirych <cirych@mail.ru>
2016-11-25 03:51:38 +0300

    init

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const {PORT} = process.env
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+    res.send('Hello World!')
 })
 
-app.listen(PORT || 3000, function () {
-  console.log('app started')
+app.listen(PORT || 3000, function() {
+    console.log('app started')
 })
