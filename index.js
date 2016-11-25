ac58056cd494749803245606ccd1206ff4973e4e
Cirych <cirych@mail.ru>
2016-11-25 05:15:36 +0300

    ready

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const express = require('express')
-const app = express()
-const moment = require('moment');
+const http = require('http')
 const {PORT} = process.env
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
-})
-
-app.get('/:date', function (req, res) {
-	const result = { "unix": null, "natural": null }
-	let date = unescape(req.params.date)
-	let unixDate = Number(date)
-	if(unixDate) date = moment.unix(unixDate)
-	else date = moment.utc(date)
-	if(date.isValid()) {
-		result.unix = date.format('X')
-		result.natural = date.format('MMMM DD, YYYY')
+const headerParser = (req, res) => {
+	let ua = req.headers["user-agent"]
+	let result = {
+		ipaddress: req.connection.remoteAddress.split(":").pop(),
+		language: req.headers["accept-language"].split(',')[0],
+		software: ua.slice(ua.indexOf('(')+1,ua.indexOf(')'))
 	}
-	res.send(result)
-})
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify(result))
+}
 
-app.listen(PORT || 3000, function() {
-    console.log('app started')
-})
+http
+	.createServer(headerParser)
+	.listen(PORT || 3000, () => console.log('started'))
